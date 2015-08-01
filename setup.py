a4f2605cf1b9b997137b70c1068357bdea8d4872
J.A. Cecil <joe.a.cecil@gmail.com>
2015-07-31 22:47:01 -0400

    Add setup.py and requirements.txt.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='eldritch_estate',
+    version='0.1.0.dev0',
+    packages=find_packages(),
+    install_requires=['pytest',
+                      'untdl']
+)
