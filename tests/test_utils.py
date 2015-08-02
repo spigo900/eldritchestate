69329bfd99df6746bcbf1b56875ad14113b27a72
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-02 15:21:55 -0400

    Use from eldestrl.utils import * in test file.
    Looks a bit cleaner, I think, since it's supposed to be testing most if
    not all of the contents of eldestrl.utils anyway.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,36 +1,37 @@
 #!/bin/env python3
-import eldestrl.utils as utils
+# import eldestrl.utils as utils
+from eldestrl.utils import *  # noqa
 
 
 def test_clamp():
-    assert utils.clamp(-50, 3, 10) == 3
-    assert utils.clamp(5, 3, 10) == 5
-    assert utils.clamp(50, 3, 10) == 10
+    assert clamp(-50, 3, 10) == 3
+    assert clamp(5, 3, 10) == 5
+    assert clamp(50, 3, 10) == 10
 
 
 def test_in_range():
-    assert utils.in_range(5, 3, 10)
-    assert not utils.in_range(-50, 3, 10)
-    assert not utils.in_range(50, 3, 10)
+    assert in_range(5, 3, 10)
+    assert not in_range(-50, 3, 10)
+    assert not in_range(50, 3, 10)
 
 
 def test_to_local_coords():
-    assert utils.to_local_coords((1, 1), (5, 5)) == (4, 4)
-    assert utils.to_local_coords((2, 10), (5, 5)) == (3, -5)
-    assert utils.to_local_coords((10, 2), (5, 5)) == (-5, 3)
-    assert utils.to_local_coords((10, 10), (5, 5)) == (-5, -5)
-    assert utils.to_local_coords((-10, -10), (5, 5)) == (15, 15)
+    assert to_local_coords((1, 1), (5, 5)) == (4, 4)
+    assert to_local_coords((2, 10), (5, 5)) == (3, -5)
+    assert to_local_coords((10, 2), (5, 5)) == (-5, 3)
+    assert to_local_coords((10, 10), (5, 5)) == (-5, -5)
+    assert to_local_coords((-10, -10), (5, 5)) == (15, 15)
 
 
 def test_subtract_iterables():
-    assert tuple(utils.subtract_iterables((1, 1), (5, 5))) == (-4, -4)
-    assert tuple(utils.subtract_iterables((2, 10), (5, 5))) == (-3, 5)
-    assert tuple(utils.subtract_iterables((10, 2), (5, 5))) == (5, -3)
-    assert tuple(utils.subtract_iterables((10, 10), (5, 5))) == (5, 5)
-    assert list(utils.subtract_iterables((-10, -10), (5, 5))) == [-15, -15]
-    assert tuple(utils.subtract_iterables([1, 1], [5, 5])) == (-4, -4)
-    assert list(utils.subtract_iterables([2, 10], [5, 5])) == [-3, 5]
-    assert list(utils.subtract_iterables([10, 2], [5, 5])) == [5, -3]
-    assert list(utils.subtract_iterables([10, 10], [5, 5])) == [5, 5]
-    assert tuple(utils.subtract_iterables([-10, -10], [5, 5])) == (-15, -15)
-    assert list(utils.subtract_iterables([-10, -10], (5, 5))) == [-15, -15]
+    assert tuple(subtract_iterables((1, 1), (5, 5))) == (-4, -4)
+    assert tuple(subtract_iterables((2, 10), (5, 5))) == (-3, 5)
+    assert tuple(subtract_iterables((10, 2), (5, 5))) == (5, -3)
+    assert tuple(subtract_iterables((10, 10), (5, 5))) == (5, 5)
+    assert list(subtract_iterables((-10, -10), (5, 5))) == [-15, -15]
+    assert tuple(subtract_iterables([1, 1], [5, 5])) == (-4, -4)
+    assert list(subtract_iterables([2, 10], [5, 5])) == [-3, 5]
+    assert list(subtract_iterables([10, 2], [5, 5])) == [5, -3]
+    assert list(subtract_iterables([10, 10], [5, 5])) == [5, 5]
+    assert tuple(subtract_iterables([-10, -10], [5, 5])) == (-15, -15)
+    assert list(subtract_iterables([-10, -10], (5, 5))) == [-15, -15]
