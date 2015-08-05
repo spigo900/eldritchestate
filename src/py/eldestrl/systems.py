05d1863813691733d739003620ffba998fb21dc0
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-05 17:47:42 -0400

    Fix update method on all systems.
    The original update methods incorrectly specified dt (time delta) as
    their first parameter and used it as the entity manager. This is
    incorrect. `ecs` systems should take the entity manager as the first
    argument and the time delta as the second. This fixes that.

--- src/py/eldestrl/systems.py
+++ src/py/eldestrl/systems.py
@@ -3,12 +3,12 @@ from ecs.exceptions import NonexistentComponentTypeForEntity
 
 
 class UpdateWorldSys(System):
-    def update(self, dt):
+    def update(self, ent_mgr, _dt):
         from eldestrl.components import World, Position
         new_ents = {}
-        for (entity, world) in dt.pairs_for_type(World):
+        for (entity, world) in ent_mgr.pairs_for_type(World):
             try:
-                coords = dt.component_for_entity(entity, Position).coords
+                coords = ent_mgr.component_for_entity(entity, Position).coords
                 new_ents[coords] = entity
             except NonexistentComponentTypeForEntity:
                 print('Entity %s has world but no position! Skipping...'
@@ -17,28 +17,38 @@ class UpdateWorldSys(System):
 
 
 class FollowEntitySys(System):
-    def update(self, dt):
+    def update(self, ent_mgr, _dt):
         from eldestrl.components import Position, Display, FollowsEntity
-        (entity, display) = next(dt.pairs_for_type(Display))
-        for (entity, follower) in dt.pairs_for_type(FollowsEntity):
-            pos = dt.component_for_entity(entity, Position)
-            followed_pos = dt.component_for_entity(follower.followed, Position)
+        (entity, display) = next(ent_mgr.pairs_for_type(Display))
+        for (entity, follower) in ent_mgr.pairs_for_type(FollowsEntity):
+            pos = ent_mgr.component_for_entity(entity, Position)
+            followed_pos = ent_mgr.component_for_entity(follower.followed,
+                                                        Position)
             pos.coords = followed_pos.coords
 
 
 class EventSys(System):
-    def update(self, dt):
+    def __init__(self):
+        self.game_ended = False
+        super(EventSys, self).__init__()
+
+    def update(self, ent_mgr, _dt):
         import untdl.event as ev
         import eldestrl.input as eldinput
         from eldestrl.components import PlayerControlled, Actor
         events = ev.get()
         for event in events:
-            for (entity, _) in dt.pairs_for_type(PlayerControlled):
+            if event.type == 'QUIT':
+                self.game_ended = True
+            for (entity, _) in ent_mgr.pairs_for_type(PlayerControlled):
                 try:
                     move_diff = eldinput.get_move_diff(event)
                     if move_diff != (0, 0):
-                        actor = dt.component_for_entity(entity, Actor)
+                        actor = ent_mgr.component_for_entity(entity, Actor)
                         actor.queue.push(('do_action_tile', move_diff))
+                    elif event.keychar == 'ESCAPE' or \
+                         event.alt and 'F4' in event.key:  # noqa
+                        ev.push(ev.Quit())
                 except AttributeError:
                     print('Got unknown event type %s!' % repr(event))
                 except NonexistentComponentTypeForEntity as err:
@@ -49,26 +59,27 @@ class EventSys(System):
 
 
 class ActorSys(System):
-    def update(self, dt):
+    def update(self, ent_mgr, _dt):
         from eldestrl.map import passable
         from eldestrl.components import Actor, World, Position, BlockMove
-        for (entity, actor) in dt.pairs_for_type(Actor):
+        for (entity, actor) in ent_mgr.pairs_for_type(Actor):
             try:
                 action = actor.queue.popleft()
                 if action[0] == 'do_action_tile':
-                    entity_position = dt.component_for_entity(entity, Position)
+                    entity_position = ent_mgr.component_for_entity(entity,
+                                                                   Position)
                     pos = entity_position.coords
                     new_pos = ((x1 + x2, y1 + y2)
                                for (x1, y1) in pos
                                for (x2, y2) in action[1])
-                    map_ = dt.component_for_entity(entity, World)
+                    map_ = ent_mgr.component_for_entity(entity, World)
                     blocked = False
                     if not passable(map_, new_pos[0], new_pos[1]):
                         blocked = True
                     else:
                         for other_ent in map_.ents:
                             try:
-                                dt.component_for_entity(entity, BlockMove)
+                                ent_mgr.component_for_entity(entity, BlockMove)
                                 blocked = True
                             except NonexistentComponentTypeForEntity:
                                 pass
@@ -79,19 +90,20 @@ class ActorSys(System):
 
 
 class RenderDisplaySys(System):
-    def update(self, dt):
+    def update(self, ent_mgr, _dt):
         from eldestrl.utils import to_local_coords
         from eldestrl.render import render_map
         from eldestrl.components import Char, Position, World, Display
         import untdl.flush
         from untdl import TDLError
-        for (display_ent, display) in dt.pairs_for_type(Display):
+        for (display_ent, display) in ent_mgr.pairs_for_type(Display):
             (display_x, display_y) = \
-                dt.component_for_entity(display_ent, Position).coords
+                ent_mgr.component_for_entity(display_ent, Position).coords
             refpoint = (display.con.width - display_x,
                         display.con.height - display_y)
             try:
-                world_map = dt.component_for_entity(display_ent, World).map_
+                world_map = ent_mgr.component_for_entity(display_ent,
+                                                         World).map_
                 con = display.con
                 con.clear()
                 render_map(con, world_map, refpoint)
@@ -99,9 +111,9 @@ class RenderDisplaySys(System):
                 print('Display entity %s has no associated world!'
                       % repr(display_ent))
             else:
-                for (entity, renderinfo) in dt.pairs_for_type(Char):
+                for (entity, renderinfo) in ent_mgr.pairs_for_type(Char):
                     try:
-                        pos = dt.component_for_entity(entity, Position)
+                        pos = ent_mgr.component_for_entity(entity, Position)
                         draw_x, draw_y = to_local_coords(refpoint, pos.coords)
                         con.draw_char(draw_x, draw_y,
                                       renderinfo.char, renderinfo.color)
