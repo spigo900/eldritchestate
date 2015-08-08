778fb50a0bb6e0191f0aa3fa1c18fd23cddcbafb
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-07 22:22:19 -0400

    Fix untdl patch script.

--- patch_untdl.sh
+++ patch_untdl.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/sh
-patch $VIRTUAL_ENV/lib/python3.4/site-packages/untdl/__init__.py untdl.patch
+patch $VIRTUAL_ENV/lib/python3.4/site-packages/untdl-0.2.2-py3.4.egg/untdl/__init__.py untdl.patch
