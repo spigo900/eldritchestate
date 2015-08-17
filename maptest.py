0b59ae0cc656cfd25285b3c3b5297f9aad4a100b
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-16 21:07:55 -0400

    Add map testing script.
    Meant to be run through the interpreter; should generate a new map and
    display the map as it's generated on an untdl (libtcod) console. Ought
    to be useful for testing the map generation algorithm.

---
+++ maptest.py
@@ -0,0 +1,46 @@
+import untdl
+import eldestrl.map as emap
+import eldestrl.render as r
+from time import sleep
+from imp import reload
+
+
+con = untdl.init(160, 90, 'test!')
+my_map = None
+
+
+def rel():
+    reload(untdl)
+    reload(emap)
+    reload(r)
+
+
+def render(map_):
+    r.render_map(con, map_, (1, 1))
+    untdl.flush()
+
+
+def fixed_render():
+    render(my_map)
+
+
+def mapgen_callback(map_):
+    print('calling!')
+    r.render_map(con, map_, (1, 1))
+    untdl.flush()
+    sleep(5)
+
+
+def new_map():
+    return emap.map_gen(emap.DEFAULT_MAP_SEED, emap.DEFAULT_MAP_INFO,
+                        mapgen_callback)
+
+
+def remap():
+    global my_map
+    my_map = new_map()
+
+sleep(0.1)
+remap()
+untdl.flush()
+sleep(15)
