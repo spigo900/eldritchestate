64df87627cd8501a5e006e3dbc39539af950de3e
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-19 17:36:46 -0400

    Fix door mixin.

--- src/py/eldestrl/tiles/mixins.py
+++ src/py/eldestrl/tiles/mixins.py
@@ -1,25 +1,26 @@
 def door(tiledefs):
     tiledefs = tiledefs.copy()
     tiledef = tiledefs[0]
-    if '_open' in tiledef['type']:
-        new_type = tiledef.copy()
-        new_type['type'] += '_closed'
-        new_type['char'] = '+'
-        new_type['blocks'] = True
-        new_type['blocks_sight'] = True
-        open_type = new_type
-        closed_type = tiledef
-    else:
+    if '_open' not in tiledef['type']:
         new_type = tiledef.copy()
         new_type['type'] += '_open'
         new_type['char'] = '/'
         new_type['blocks'] = False
         new_type['blocks_sight'] = False
+        open_type = new_type
+        closed_type = tiledef
+    else:
+        new_type = tiledef.copy()
+        new_type['type'] = new_type['type'].replace("_open", "")
+        new_type['type'] += '_closed'
+        new_type['char'] = '+'
+        new_type['blocks'] = True
+        new_type['blocks_sight'] = True
         open_type = tiledef
         closed_type = new_type
     open_type.setdefault('behaviors', {})['close'] = \
-        ('!change_tile_type', closed_type['type'])
+        ('change_tile_type', closed_type['type'])
     closed_type.setdefault('behaviors', {})['open'] = \
-        ('!change_tile_type', open_type['type'])
+        ('change_tile_type', open_type['type'])
     tiledefs.append(new_type)
     return tiledefs
