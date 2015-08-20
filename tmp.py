8e53be3c52aa2e327eb939b21af8c27a7e3c6e8b
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-20 17:05:12 -0400

    Refactor temporary code somewhat.

--- tmp.py
+++ tmp.py
@@ -1,18 +1,41 @@
+class LineChar:
+    HORIZONTAL_THIN = b'\xC4'
+    VERTICAL_THIN = b'\xB3'
+    HORIZONTAL_THICK = b'\xCD'
+    VERTICAL_THICK = b'\xBA'
+
+
+def draw_horiz(con, x1, x2, y, char):
+    for x in range(min(x1, x2), max(x1, x2) + 1):
+        con.draw_char(x, y, char)
+
+
+def draw_vert(con, y1, y2, x, char):
+    for y in range(min(y1, y2), max(y1, y2) + 1):
+        con.draw_char(x, y, char)
+
+
 def draw_box(con, x, y, width, height, fg=(255, 255, 255), bg=None, fill=True):
     """Draw a box on the console."""
-    con.draw_char(x, y, b'\xDA', fg, bg)  # upper left corner
-    con.draw_char(x + width - 1, y, b'\xBF', fg, bg)  # upper right corner
-    con.draw_char(x, y + height - 1, b'\xC0', fg, bg)  # lower left corner
-    con.draw_char(x + width - 1, y + height - 1, b'\xD9', fg, bg)  # lower right corner
+    # upper left corner
+    con.draw_char(x, y, b'\xDA', fg, bg)
+    # upper right corner
+    con.draw_char(x + width - 1, y, b'\xBF', fg, bg)
+    # lower left corner
+    con.draw_char(x, y + height - 1, b'\xC0', fg, bg)
+    # lower right corner
+    con.draw_char(x + width - 1, y + height - 1, b'\xD9', fg, bg)
     horiz_start = x + 1
     for i in range(0, width - 2):
-        con.draw_char(horiz_start + i, y, b'\xC4', fg, bg)  # horizontal sections
-        con.draw_char(horiz_start + i, y + height - 1, b'\xC4', fg, bg)
+        con.draw_char(horiz_start + i, y, LineChar.HORIZONTAL_THIN, fg, bg)
+        con.draw_char(horiz_start + i, y + height - 1,
+                      LineChar.HORIZONTAL_THIN, fg, bg)
 
     vert_start = y + 1
     for i in range(0, height - 2):
-        con.draw_char(x, vert_start + i, b'\xB3', fg, bg)  # vertical sections
-        con.draw_char(x + width - 1, vert_start + i, b'\xB3', fg, bg)
+        con.draw_char(x, vert_start + i, LineChar.VERTICAL_THIN, fg, bg)
+        con.draw_char(x + width - 1, vert_start + i,
+                      LineChar.VERTICAL_THIN, fg, bg)
 
     if fill:
         con.draw_rect(1, 1, width - 2, height - 2, ' ', bgcolor=bg)
@@ -27,13 +50,15 @@ def draw_box_thick(con, x, y, width, height,
     con.draw_char(x + width - 1, y + height - 1, b'\xBC', fg, bg)
     horiz_start = x + 1
     for i in range(0, width - 2):
-        con.draw_char(horiz_start + i, y, b'\xCD', fg, bg)
-        con.draw_char(horiz_start + i, y + height - 1, b'\xCD', fg, bg)
+        con.draw_char(horiz_start + i, y, LineChar.HORIZONTAL_THICK, fg, bg)
+        con.draw_char(horiz_start + i, y + height - 1,
+                      LineChar.HORIZONTAL_THICK, fg, bg)
 
     vert_start = y + 1
     for i in range(0, height - 2):
-        con.draw_char(x, vert_start + i, b'\xBA', fg, bg)
-        con.draw_char(x + width - 1, vert_start + i, b'\xBA', fg, bg)
+        con.draw_char(x, vert_start + i, LineChar.VERTICAL_THICK, fg, bg)
+        con.draw_char(x + width - 1, vert_start + i,
+                      LineChar.VERTICAL_THICK, fg, bg)
 
     if fill:
         con.draw_rect(1, 1, width - 2, height - 2, ' ', bgcolor=bg)
