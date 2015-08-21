5e03f6894a61f805a75d60b29741ad9a940d6584
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-21 14:42:13 -0400

    Split lighting color modification into its own fn.
    Should be more resistant to changes in what lighting values are stored
    in the lighting map, I think. Also, less code duplication.

--- src/py/eldestrl/render.py
+++ src/py/eldestrl/render.py
@@ -1,6 +1,17 @@
 import eldestrl.map as gmap
 
 
+def lit_color(color, lighting):
+    """Return the color as modified by the lighting.
+
+    color should be any tuple of 3 ints.
+    lighting should be whatever values are stored in light_map as values.
+    """
+    light_mult = (lighting,) * 3
+    return tuple(int(n * k)
+                 for n, k in zip(color, light_mult))
+
+
 # rendering
 def render_msgs(con, coords, msgs, n=5):
     '''Takes a console, a coordinate pair, a sliceable collection of messages
@@ -18,21 +29,16 @@ def render_map(con, map_, refpoint, fov):
                        coord[1] - refpoint[1])
         if draw_coords in con and coord in fov:
             try:
+                tile_lighting = map_.light_map[coord]
                 tile_info = gmap.get_tile_type(map_, tile_type)
                 tile_char = tile_info['char']
                 tile_fg = tile_info.get('color', (255, 255, 255))
                 tile_bg = tile_info.get('bg_color', None)
-                tile_bg_final = tuple(int(n * k)
-                                      for n, k in
-                                      zip(tile_bg,
-                                          (map_.light_map[coord],) * 3)) \
+                tile_bg_final = lit_color(tile_bg, tile_lighting) \
                     if tile_bg else tile_bg
                 con.draw_char(draw_coords[0], draw_coords[1],
                               tile_char,
-                              tuple(int(n * k)
-                                    for n, k in
-                                    zip(tile_fg,
-                                        (map_.light_map[coord],) * 3)),
+                              lit_color(tile_fg, tile_lighting),
                               tile_bg_final)
             except AttributeError:
                 print('ERROR! Tile type %s does not exist'
