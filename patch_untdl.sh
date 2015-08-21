05d2444d0bf8e3b3e7a3ae421c6e62c066e7e247
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-21 15:40:09 -0400

    Add new patch for UnTDL.
    UnTDL's noise generator should now work correctly, rather than core
    dumping on me.

--- patch_untdl.sh
+++ patch_untdl.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/sh
-patch $VIRTUAL_ENV/lib/python3.4/site-packages/untdl-0.2.2-py3.4.egg/untdl/__init__.py untdl.patch
+patch "$@" $VIRTUAL_ENV/lib/python3.4/site-packages/untdl/__init__.py < untdl.patch
+patch "$@" $VIRTUAL_ENV/lib/python3.4/site-packages/untdl/__tcod.py < untdl_noise.patch
