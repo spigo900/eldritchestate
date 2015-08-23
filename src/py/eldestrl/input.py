75b5c71182fb8f43108c62911669978574b3ecf8
J.A. Cecil <joe.a.cecil@gmail.com>
2015-08-23 15:11:33 -0400

    Rename MOVE_CONTROLS_MAP to CONTROLS_MAP.
    Slightly more accurate to what it's actually used for right now.

--- src/py/eldestrl/input.py
+++ src/py/eldestrl/input.py
@@ -1,29 +1,29 @@
 from eldestrl.utils import get_event_key
 
 
-MOVE_CONTROLS_MAP = {'h': ('do_action_tile', (-1, 0)),
-                     'j': ('do_action_tile', (0, 1)),
-                     'k': ('do_action_tile', (0, -1)),
-                     'l': ('do_action_tile', (1, 0)),
-                     'y': ('do_action_tile', (-1, -1)),
-                     'u': ('do_action_tile', (1, -1)),
-                     'b': ('do_action_tile', (-1, 1)),
-                     'n': ('do_action_tile', (1, 1)),
-                     'H': ('run_dir', (-1, 0)),
-                     'J': ('run_dir', (0, 1)),
-                     'K': ('run_dir', (0, -1)),
-                     'L': ('run_dir', (1, 0)),
-                     'Y': ('run_dir', (-1, -1)),
-                     'U': ('run_dir', (1, -1)),
-                     'B': ('run_dir', (-1, 1)),
-                     'N': ('run_dir', (1, 1)),
-                     'ESCAPE': ('quit',)}
+CONTROLS_MAP = {'h': ('do_action_tile', (-1, 0)),
+                'j': ('do_action_tile', (0, 1)),
+                'k': ('do_action_tile', (0, -1)),
+                'l': ('do_action_tile', (1, 0)),
+                'y': ('do_action_tile', (-1, -1)),
+                'u': ('do_action_tile', (1, -1)),
+                'b': ('do_action_tile', (-1, 1)),
+                'n': ('do_action_tile', (1, 1)),
+                'H': ('run_dir', (-1, 0)),
+                'J': ('run_dir', (0, 1)),
+                'K': ('run_dir', (0, -1)),
+                'L': ('run_dir', (1, 0)),
+                'Y': ('run_dir', (-1, -1)),
+                'U': ('run_dir', (1, -1)),
+                'B': ('run_dir', (-1, 1)),
+                'N': ('run_dir', (1, 1)),
+                'ESCAPE': ('quit',)}
 
 
 def get_action_key(key_event):
     '''Get the action tuple for a given key.'''
     key = get_event_key(key_event)
-    return MOVE_CONTROLS_MAP.get(key, (None,))
+    return CONTROLS_MAP.get(key, (None,))
 
 
 def get_action(event):
