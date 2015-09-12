0f39ab6469fa19cae2d134f7247ed32360c45574
J.A. Cecil <joe.a.cecil@gmail.com>
2015-09-12 10:48:26 -0400

    Add some new tree operations.
    Some of these, I'll probably delete.

--- src/py/eldestrl/qtree.py
+++ src/py/eldestrl/qtree.py
@@ -13,6 +13,31 @@ def new_leaf(val):
     return [val, [], [], [], []]
 
 
+def tree_value(tree):
+    return tree[0]
+
+
+def tree_children(tree):
+    return (child for child in tree[1:] if child)
+
+
+def tree_append(tree, val):
+    for idx, child in enumerate(tree[1:5]):
+        if not child and val not in tree[1:5]:
+            tree[idx + 1] = val
+            # appended = True
+    # if not appended:
+    if val not in tree[1:5]:
+        raise IndexError("No empty spaces in quad tree!")
+    # assert val in tree
+
+
+def tree_cons(tree, val):
+    treec = tree.copy()
+    tree_append(treec, val)
+    return treec
+
+
 def reduce(f, acc, tree):
     if not tree:
         return acc
@@ -84,7 +109,7 @@ def qmapi2(tree, f):
 def is_branch(node):
     try:
         node[1]
-        return any(True for v in node[1:] if v is not None)
+        return any(v != [] for v in node[1:])
     except IndexError:
         return False
 
