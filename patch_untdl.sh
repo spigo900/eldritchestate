14c7056b3cba7143a21a8072d60e9e8feda3f91e
J.A. Cecil <joe.a.cecil@gmail.com>
2016-08-27 14:43:03 -0400

    Make patch_untdl slightly more flexible.

--- patch_untdl.sh
+++ patch_untdl.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
-patch "$@" $VIRTUAL_ENV/lib/python3.4/site-packages/untdl/__init__.py < untdl.patch
-patch "$@" $VIRTUAL_ENV/lib/python3.4/site-packages/untdl/__tcod.py < untdl_noise.patch
+PYTHON_VERSION=$(python3 -V | sed -r 's/.*?(3\.[0-9]).*/\1/')
+patch "$@" $VIRTUAL_ENV/lib/python$PYTHON_VERSION/site-packages/untdl/__init__.py < untdl.patch
+patch "$@" $VIRTUAL_ENV/lib/python$PYTHON_VERSION/site-packages/untdl/__tcod.py < untdl_noise.patch
